015b66d7134660c624fdf4eb2cbac40d19c7e10e
Destino <111420909+Destino233@users.noreply.github.com>
2023-08-01 20:26:32 +0800

    Update contribute.tsx

--- Website-main/src/pages/contribute.tsx
+++ Website-main/src/pages/contribute.tsx
@@ -7,21 +7,21 @@ import SavingsIllustration from "@/assets/illustrations/undraw/savings.svg";
 import Button from "@/components/input/Button";
 import SEO from "@/components/util/SEO";
 
-const Contribute: NextPage = () => (
+const MinecraftServerFAQ: NextPage = () => (
   <>
     <SEO
-      title="Contributing"
-      description="Without contributors our projects wouldn't be possible. Find out how you can help."
-      keywords={["leavesmc", "leaves", "minecraft", "sponsor", "contributing"]}
+      title="Frequently Asked Questions"
+      description="Find answers to frequently asked questions about our Minecraft server."
+      keywords={["minecraft", "faq", "server"]}
     />
     <header className="max-w-7xl flex flex-row mx-auto px-4 pt-32 pb-16 lg:(pt-48 pb-32) gap-16">
       <div className="flex-1">
         <h1 className="font-medium leading-normal lg:(text-5xl leading-normal) text-4xl">
-          You can contribute
+          Minecraft 服务器常见问题解答
         </h1>
         <p className="text-xl mt-4">
           {
-            "Our projects are based on community contributions and wouldn't be possible without them. There are a lot of ways to contribute, even without programming knowledge."
+            "在这里，您可以了解到有关我们的 Minecraft 服务器的常见问题及其解答。"
           }
         </p>
         <div className="flex flex-row gap-4 mt-8"></div>
@@ -29,17 +29,17 @@ const Contribute: NextPage = () => (
       <div className="flex-1 lg:flex hidden justify-end"></div>
     </header>
     <section
-      id="donate"
+      id="rules"
       className="px-4 py-4 max-w-7xl mx-auto flex flex-col-reverse items-center gap-8 lg:flex-row"
     >
       <div className="flex-1">
-        <h2 className="font-medium text-2xl md:text-4xl">...Financially</h2>
+        <h2 className="font-medium text-2xl md:text-4xl">服务器规则</h2>
         <p className="md:(mt-6 text-lg) text-gray-900 dark:text-gray-100 mt-3">
-          LeavesMC 使用 Pateron 和 AFDIAN 管理各种循环支出，主要涉及基础设施。
+          请确保您已阅读并了解我们的服务器规则，以确保服务器中的良好游戏体验。
         </p>
         <div className="flex flex-row gap-4 mt-8">
-          <Button variant="filled" href="/sponsors" dense>
-            了解更多
+          <Button variant="filled" href="/rules" dense>
+            查看规则
           </Button>
         </div>
       </div>
@@ -48,22 +48,22 @@ const Contribute: NextPage = () => (
       </div>
     </section>
     <section
-      id="code"
+      id="connect"
       className="px-4 py-4 max-w-7xl mx-auto flex flex-col-reverse items-center mt-12 gap-8 lg:flex-row"
     >
       <div className="flex-1">
-        <h2 className="font-medium text-2xl md:text-4xl">...Code</h2>
+        <h2 className="font-medium text-2xl md:text-4xl">如何连接服务器</h2>
         <p className="md:(mt-6 text-lg) text-gray-900 dark:text-gray-100 mt-3">
-          我们项目中的大部分代码都是由社区成员编写的，我们欢迎你的贡献！
+          以下是连接我们的 Minecraft 服务器所需的步骤和信息。
         </p>
         <div className="flex flex-row gap-4 mt-8">
           <Button
             variant="filled"
-            href="https://github.com/LeavesMC/Leaves/blob/master/CONTRIBUTING.md"
+            href="https://docs.destino.top/minecraft/connect"
             external
             dense
           >
-            了解更多
+            查看教程
           </Button>
         </div>
       </div>
@@ -76,18 +76,36 @@ const Contribute: NextPage = () => (
       className="px-4 py-4 max-w-7xl mx-auto flex flex-col-reverse items-center mt-12 gap-8 lg:flex-row"
     >
       <div className="lg:flex-1">
-        <h2 className="font-medium text-2xl md:text-4xl">...Support</h2>
+        <h2 className="font-medium text-2xl md:text-4xl">技术支持</h2>
         <p className="md:(mt-6 text-lg) text-gray-900 dark:text-gray-100 mt-3">
-          大量用户意味着大量问题。每个人都可以在我们的 Discord 服务器或论坛上回答问题或提供支持。
+          如果您在 Minecraft 服务器上遇到问题，请参考以下技术支持信息。
         </p>
         <div className="flex flex-row gap-4 mt-8">
          <Button
             variant="filled"
-            href="https://discord.gg/leaves"
+            href="https://docs.destino.top/minecraft/support"
             external
             dense
           >
-            了解更多
+            查看文档
+          </Button       </div>
+      </div>
+      <div className="justify-start flex w-full lg:(flex-1 justify-end)">
+        <KnowledgeIllustration className="max-h-32 lg:(max-w-72 max-h-none)" />
+      </div>
+    </section>
+    <section
+      id="chat"
+      className="px-4 py-4 max-w-7xl mx-auto flex flex-col-reverse items-center mt-12 gap-8 lg:flex-row"
+    >
+      <div className="lg:flex-1">
+        <h2 className="font-medium text-2xl md:text-4xl">聊天规则</h2>
+        <p className="md:(mt-6 text-lg) text-gray-900 dark:text-gray-100 mt-3">
+          请阅读我们的聊天规则，以确保在服务器上保持尊重和秩序。
+        </p>
+        <div className="flex flex-row gap-4 mt-8">
+          <Button variant="filled" href="/rules#chat" dense>
+            查看规则
           </Button>
         </div>
       </div>
@@ -95,31 +113,5 @@ const Contribute: NextPage = () => (
         <ChattingIllustration className="max-h-32 lg:(max-w-72 max-h-none)" />
       </div>
     </section>
-    <section
-      id="docs"
-      className="px-4 py-4 max-w-7xl mx-auto flex flex-col-reverse items-center mt-12 gap-8 lg:flex-row"
-    >
-      <div className="flex-1">
-        <h2 className="font-medium text-2xl md:text-4xl">...Docs</h2>
-        <p className="md:(mt-6 text-lg) text-gray-900 dark:text-gray-100 mt-3">
-          文档是维护项目的重要组成部分。如果你发现文档中的错误或有改进建议，请告诉我们！
-        </p>
-        <div className="flex flex-row gap-4 mt-8">
-          <Button
-            variant="filled"
-            href="https://github.com/LeavesMC/LeavesDocs/blob/master/CONTRIBUTING.md"
-            external
-            dense
-          >
-            了解更多
-          </Button>
-        </div>
-      </div>
-      <div className="justify-start flex w-full lg:(flex-1 justify-end)">
-        <KnowledgeIllustration className="max-h-32 lg:(max-w-72 max-h-none)" />
-      </div>
-    </section>
   </>
 );
-
-export default Contribute;
